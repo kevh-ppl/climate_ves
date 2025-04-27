8459e32073551e222caaecca7f2251d5b02914fb
Kevin Haro <kevin13haro@gmail.com>
2025-04-26 19:34:32 -0600

    feat(Navbar) => Navbar y prueba de img

---
+++ src/app/inicio/inicio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+
+@Component({
+  selector: 'app-inicio',
+  imports: [NavbarComponent],
+  templateUrl: './inicio.component.html',
+  styleUrl: './inicio.component.css'
+})
+export class InicioComponent {
+
+}
