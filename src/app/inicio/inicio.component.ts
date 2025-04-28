e41e7226774271491ca792a0496dadc544d0a942
Kevin Haro <kevin13haro@gmail.com>
2025-04-27 23:05:48 -0600

    feat(timeline) => Time line en inicio

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { QueEsComponent } from "../que-es/que-es.component";
 import { StatisticsComponent } from "../statistics/statistics.component";
+import { TimelineComponent } from "../timeline/timeline.component";
 
 @Component({
   selector: 'app-inicio',
-  imports: [NavbarComponent, QueEsComponent, StatisticsComponent],
+  imports: [NavbarComponent, QueEsComponent, StatisticsComponent, TimelineComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
