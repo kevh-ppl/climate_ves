f60e815d5ede29bfb64ab9442e96fa7d14529826
Kevin Haro <kevin13haro@gmail.com>
2025-04-27 23:32:37 -0600

    styke

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -3,10 +3,11 @@ import { NavbarComponent } from "../navbar/navbar.component";
 import { QueEsComponent } from "../que-es/que-es.component";
 import { StatisticsComponent } from "../statistics/statistics.component";
 import { TimelineComponent } from "../timeline/timeline.component";
+import { ActionComponent } from "../action/action.component";
 
 @Component({
   selector: 'app-inicio',
-  imports: [NavbarComponent, QueEsComponent, StatisticsComponent, TimelineComponent],
+  imports: [NavbarComponent, QueEsComponent, StatisticsComponent, TimelineComponent, ActionComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
