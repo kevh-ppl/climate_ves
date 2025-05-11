61939dd11e6b57d706c27071389ced5fef112af2
Kevin Haro <kevin13haro@gmail.com>
2025-05-11 14:09:54 -0600

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,7 +2,7 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/extra_proyect',
+  baseHref: '/extra_proyect/',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {
@@ -14,8 +14,8 @@ export default {
   ]
 },
   assets: {
-    'index.csr.html': {size: 28085, hash: '273582f171b0d550a8e7df052ad043df818fe892926c85d32d08bb788a77c2de', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17204, hash: 'bd6adbd25c70e927f34507f5b5b82c866f60f2b82b495a8df639ae25523034bb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 28086, hash: '54cbe303a3c0ac5f19769521f98b9349bc8315e19be7c03ec1776b5ae1d3cb6f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17205, hash: 'c79fc306b025806543357b6fa595d7ba551d00ba224949ad79d0c712ad185fa1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-DLBGDHWB.css': {size: 237885, hash: 'EFXg02ETPfQ', text: () => import('./assets-chunks/styles-DLBGDHWB_css.mjs').then(m => m.default)}
   },
 };
